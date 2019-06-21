d85240790ea4cccc96ece0d5b6d2db624be4e98b
rushgeo <10775395+rushgeo@users.noreply.github.com>
2019-06-21 12:41:03 -0400

    moved functions to another script

--- code/aeronet_drake.R
+++ code/aeronet_drake.R
@@ -11,58 +11,102 @@
 # ? which other columns to we want?
 # restrict to 2000+ (parameter so that different subsets could be taken later)
 
+# Libraries  ~~~~~~~~~~~~~~~~~~~~~~ ####
+library(sf)
+library(magrittr)
+library(drake)
+library(data.table)
+library(fst)
+library(future)
+#install.packages("/home/rushj03/dev/nngeo", repos = NULL, type = "source")
+library(nngeo, lib.loc = "~/R/x86_64-pc-linux-gnu-library/3.6")
+#library(sp)
+#library(rgdal)
+#library(rnaturalearth) # REPLACE WITH CONUS FILE USED IN GRID XXX
+#library(rgeos)
+
 # Parameters ~~~~~~~~~~~~~~~~~~~~~~ ####
 
 aer_stn_path = '~/qnap_geo/aeronet/aeronet_locations_v3_20180604.txt'
 aer_data_path = '~/qnap_geo/aeronet/All_Sites_Times_All_Points_AOD20_20180603.dat'
 aer_data_path = '/scratch/temp/All_Sites_Times_All_Points_AOD20_20180603.dat' # don't have time to play with Ethernet all day
-date_start = "2000-01-01" # do not include in sel_data_bytime if don't want to limit
+#date_start = "2000-01-01" # do not include in sel_data_bytime if don't want to limit
+date_start = "2018-01-01" # do not include in sel_data_bytime if don't want to limit
 date_end   = "2018-12-31" # do not include in sel_data_bytime if don't want to limit
 geo_region = "NEMIA"
 aer_columns = c("AERONET_Site", "Date(dd:mm:yyyy)", "Time(hh:mm:ss)", "Day_of_Year", "AOD_440nm", 
-                "Site_Latitude(Degrees)", "Site_Longitude(Degrees)", # remove later, curious if there are mobile stations
+                #"Site_Latitude(Degrees)", "Site_Longitude(Degrees)", # confirmed stations do not move!
                 "Solar_Zenith_Angle(Degrees)", "Precipitable_Water(cm)")
 
 ssd_cache = new_cache(path = "/scratch/cache/aeronet_drake") # currently on Belle
 
-# Functions  ~~~~~~~~~~~~~~~~~~~~~~ ####
+# Functions ~~~~~~~~~~~~~~~~~~~~ 
 
-get_stn_data <- function(data_path, keep_columns){
-  dt = fread(data_path, select = keep_columns, fill = TRUE, skip = 6)
-  dt[, day := as.POSIXct(paste(`Date(dd:mm:yyyy)`, `Time(hh:mm:ss)`), 
-                                 format = "%d:%m:%Y %H:%M:%S", tz = "UTC")]
-  dt[, c("Date(dd:mm:yyyy)", "Time(hh:mm:ss)") := NULL]
-  return(dt)
-}
-sel_data_bytime <- function(aer_data, date_start = NULL, date_end = NULL){
-  if(!is.null(date_start)){ aer_data = aer_data[day >= as.POSIXct(date_start, tz = "UTC"), ] }
-  if(!is.null(date_end)){   aer_data = aer_data[day <= as.POSIXct(date_end, tz = "UTC"),   ] }
-  return(aer_data)
-}
+source("code/aeronet_processing.R")
 
 # Drake Plans  ~~~~~~~~~~~~~~~~~~~~ ####
 
 nemia_plan <- drake_plan(
+  # station locations
   aer_stns = fread(file = file_in(aer_stn_path), col.names = c("Site_Name", "lon", "lat", "elevm")),
+  conus_buff = get_conus_buff(),
+  nemia_buff = get_nemia_buff(),
+  aerpts = get_aer_spatial(aer_stns),               # wgs84
+  conus_aer = select_points(aerpts, conus_buff),    
+  nemia_aer = select_points(conus_aer, nemia_buff), 
+  
+  # get nearest MODIS cells 
+  refgrid = get_ref_grid(),
+  candidate_refpts = points_in_buffer(nemia_aer, refgrid),
+  aer_nearest = get_nearest_cell(nemia_aer, candidate_refpts),
+  
+  # measurements. Editing values of date_start and date_end should make use of the cache if range was previously calc'd
   aer_data = get_stn_data(file_in(aer_data_path), aer_columns),
   aer_btw = target(sel_data_bytime(aer_data, date_start, date_end),
-                    transform = map(date_start = !!date_start, date_end = !!date_end))
+                   transform = map(date_start = !!date_start, date_end = !!date_end)),
+  
+  # selected measurements, transform by same date range if not automatic in name
+  nemia_data = target(sel_data_bystation(aer_btw, nemia_aer),
+                      transform = map(date_start = !!date_start, date_end = !!date_end)),
+  
+  # joined mcd19 MAIAC values. transform by same date range if not automatic in name
+  
 )
 
-# note: line 25564581 out of 26171185 ends early. possibly others, but that's where fread warned
-# 329 s for aer_data last time from QNAP, 108s from SSD
+
 
 nemia_plan
-drake_plan_source(nemia_plan)
-
+#drake_plan_source(nemia_plan)
 nemia_config <- drake_config(nemia_plan, cache = ssd_cache)
-vis_drake_graph(nemia_config, from = names(nemia_config$layout))
+#vis_drake_graph(nemia_config, from = names(nemia_config$layout))
 vis_drake_graph(nemia_config)
 
 outdated(nemia_config)
+plan("multicore")
 make(nemia_plan, cache = ssd_cache)
 cached(cache = ssd_cache)
 
+# testing ####
+loadd(candidate_refpts, cache = ssd_cache)
+loadd(nemia_aer, cache = ssd_cache)
+mapview(candidate_refpts, col.regions = "blue") + mapview(nemia_aer, col.regions = "red")
+loadd(aer_nearest, cache = ssd_cache)
 
 
 
+
+# notes ####
+# 2000-2018: 174 unique NEMIA sites, 130 in the data
+
+# old notes ####
+
+## doesn't look like separate plans are possible; maybe fork and edit drake_plan() sometime
+# data_plan <- drake_plan(
+#   nemia_plan, # would need to expand this to the rows
+#   aer_data = get_stn_data(file_in(aer_data_path), aer_columns),
+#   aer_btw = target(sel_data_bytime(aer_data, date_start, date_end),
+#                    transform = map(date_start = !!date_start, date_end = !!date_end)) )
+
+
+# note: line 25564581 out of 26171185 ends early. possibly others, but that's where fread warned
+# 329 s for aer_data last time from QNAP, 108s from SSD
