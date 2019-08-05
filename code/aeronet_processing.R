0d2fd44bf2484a02beea2f4bdd8704702ee473b8
liuyanguu <lyhello@gmail.com>
2019-08-05 11:56:24 -0400

    [aeronet_drake]remove two stations on water

--- code/aeronet_processing.R
+++ code/aeronet_processing.R
@@ -1,24 +1,26 @@
 # Functions for aeronet_drake.R ~~~~~~~~~~~~~~~~~~~~~~ ####
 
-get_stn_data <- function(data_path, keep_columns){
-  dt = fread(data_path, select = keep_columns, fill = TRUE, skip = 6)
-  dt[, day := as.POSIXct(paste(`Date(dd:mm:yyyy)`, `Time(hh:mm:ss)`), 
-                         format = "%d:%m:%Y %H:%M:%S", tz = "UTC")]
-  dt[, c("Date(dd:mm:yyyy)", "Time(hh:mm:ss)") := NULL]
-  return(dt)
+
+# 1. Prepare subset of Aeronet stations ------------------------------------------
+
+#' modify_aer_stns: Modify AERONET sites (dt) to remove sites on water
+#' @param aer_stns0 df of AERONET stations 
+#' @return a new df
+modify_aer_stns <- function(aer_stns0){
+  aer_stns0 <- aer_stns0[!Site_Name%in%c("MVCO", "LISCO")]
+  return(aer_stns0)
 }
-sel_data_bytime <- function(aer_data, date_start = NULL, date_end = NULL){
-  if(!is.null(date_start)){ aer_data = aer_data[day >= as.POSIXct(date_start, tz = "UTC"), ] }
-  if(!is.null(date_end)){   aer_data = aer_data[day <= as.POSIXct(date_end, tz = "UTC"),   ] }
-  return(aer_data)
-}
-get_ref_grid <- function(ref_file = "/data-belle/LST/MODIS.LST.C6/derived/conus_grid_201906.fst"){
-  refDT = read_fst(ref_file, as.data.table = TRUE, columns = c("idLSTpair0", "x_wgs84", "y_wgs84"))
-  st_as_sf(refDT, coords = c("x_wgs84", "y_wgs84"), crs = 4326)
-}
+
+#' get_conus_buff
+#' @param conus_file location of the sf CONUS file no Great Lake -- rds file
+#' @return sf object of CONUS shapfile
 get_conus_buff <- function(conus_file = "/data-belle/LST/MODIS.LST.C6/derived/conus_sf_noGLakes.rds"){
   readRDS(conus_file)
 }
+
+#' get_nemia_buff
+#' @param states_file US states shp file
+#' @return sf object of NEMIA shapfile
 get_nemia_buff <- function(states_file = "/data-belle/census/states/tl_2017_us_state.shp"){
   # no longer using the old 2km buffer of NEMIA; this is same data source used for CONUS grid
   states = st_read(states_file)
@@ -27,14 +29,26 @@ get_nemia_buff <- function(states_file = "/data-belle/census/states/tl_2017_us_s
                                       "DE", "MD", "DC", "VA", "WV"), ]
   nemia = st_union(nemia)
 }
+
+#' Turn Aeronet station data.table into sf project
+#' @param aer_stns d.f of AERONET stations, "Site_Name", "lon", "lat", "elevm" 
+#' @param sf default to TRUE, make a sf object, if FALSE, make sp object 
+#' @return sf object or sp SpatialPointsDataFrame 
+#' 
 get_aer_spatial <- function(aer_stns, sf = TRUE){
   if(sf){
-    st_as_sf(aer_stns, coords = c("lon", "lat"), crs = 4326)
+    sf::st_as_sf(aer_stns, coords = c("lon", "lat"), crs = 4326)
   }else{
-    SpatialPointsDataFrame(coords = aer_stns[, c("lon", "lat")], data = aer_stns, 
-                           proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+    sp::SpatialPointsDataFrame(coords = aer_stns[, c("lon", "lat")], data = aer_stns, 
+                           proj4string = sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
   }
 }
+
+#' select points by st_intersects points(aerpts) with polygon(reg_polygon)
+#' @param aerpts geometry points
+#' @param reg_polygon geometry polygon
+#' @return aerpts: a subset of geometry points
+#'
 select_points <- function(aerpts, reg_polygon){
   if(st_crs(aerpts) != st_crs(reg_polygon)){ # polygons are in nad83
     aerpts <- st_transform(aerpts, crs = st_crs(reg_polygon))
@@ -43,43 +57,65 @@ select_points <- function(aerpts, reg_polygon){
   if(st_crs(aerpts) != st_crs(4326)){ aerpts <- st_transform(aerpts, crs = 4326) } # return in wgs84
   aerpts
 }
+
+
+#' read in reference grid, and turn into geometry points
+#' @param ref_file file dir of the grid file in .fst
+#' @return sf object of grid in WGS84
+#' 
+get_ref_grid <- function(ref_file = "/data-belle/LST/MODIS.LST.C6/derived/conus_grid_201906.fst"){
+  refDT = read_fst(ref_file, as.data.table = TRUE, columns = c("idLSTpair0", "x_wgs84", "y_wgs84"))
+  st_as_sf(refDT, coords = c("x_wgs84", "y_wgs84"), crs = 4326)
+}
+
+#' read in the Aeronet AOD measurement data from `data_path`
+get_stn_data <- function(data_path, keep_columns){
+  dt = fread(data_path, select = keep_columns, fill = TRUE, skip = 6)
+  dt[, day := as.POSIXct(paste(`Date(dd:mm:yyyy)`, `Time(hh:mm:ss)`), 
+                         format = "%d:%m:%Y %H:%M:%S", tz = "UTC")]
+  dt[, c("Date(dd:mm:yyyy)", "Time(hh:mm:ss)") := NULL]
+  return(dt)
+}
+
+
+#' find points in buffer within dist
+#' @param aerpts The NEMIA Aeronet sites points 
+#' @param refpts The loaded referecen grid points by \code{\function{get_ref_grid}}  
+#' @return candidate_refpts
+#' 
 points_in_buffer <- function(aerpts, refpts, dist = 1500){
   aerptsNA <- st_transform(aerpts, crs = 2163) # US National Atlas
   aer_regions <- aerptsNA %>% st_buffer(dist = dist) %>% st_union
   refptsNA <- st_transform(refpts, crs = 2163) # US National Atlas
   refsub <- refpts[st_intersects(refptsNA, aer_regions, sparse = FALSE), ]
-  #st_transform(refsub, crs = 4326) # return in wgs84
+  st_transform(refsub, crs = 4326) # return in wgs84
 }
+
+
+#' match the nearest grid point to Aeronet sites  
+#' @param aerpts The NEMIA Aeronet sites points 
+#' @param candidate_refpts all the points_in_buffer
 get_nearest_cell <- function(aerpts, candidate_refpts){
   nn_aer_ref <- st_nn(aerpts, candidate_refpts, k = 1, returnDist = FALSE)
   # join LSTids to stations
   aerpts$nearest_refgrid <- candidate_refpts[unlist(nn_aer_ref), ]$idLSTpair0
   aerpts
 }
+remove_site_on_water <- function(aer_nearest){
+  aer_nearest[!aer_nearest$Site_Name%in%c("MVCO", "LISCO"),]
+}
+
+
+# 2. prepare AOD measurement data -------------------------------------------------
+# select AOD data by station and date 
 sel_data_bystation <- function(aer_data, aer_stns){
-  # aer_stns expected to be sf points, but coule be a data.table as long as it has column Site_Name
+  # aer_stns expected to be sf points, but could be a data.table as long as it has column Site_Name
   aer_data[AERONET_Site %in% aer_stns$Site_Name]
 }
 
-# mcd19 processing ####
 
-# open matching years of mcd19 data, Aqua and Terra if 2002+, extracting data for the closest cells to AERONET stations
-
-
-# join_mcd19(mcd19path, date_start, date_end, aer_nearest, aer_data){
-#   # get years in the date range
-#   
-#   # loop over years (xxx append year to path)
-#   
-#   # do Terra
-#     # matching files, get matching cells for all
-#     # do the join - check times
-#   
-#   # if 2002+, do Aqua (adding columns to Terra table)
-#     # do in parallel with Terra? Make this loop an expanded set of targets by year?
-#   
-#    
-# }
-# get_mcd19_day(path, cells){
-#   
-# }
+sel_data_bytime <- function(aer_data, date_start = NULL, date_end = NULL){
+  if(!is.null(date_start)){ aer_data = aer_data[day >= as.POSIXct(date_start, tz = "UTC"), ] }
+  if(!is.null(date_end)) {aer_data = aer_data[day <= as.POSIXct(date_end, tz = "UTC"),   ] }
+  return(aer_data)
+}
