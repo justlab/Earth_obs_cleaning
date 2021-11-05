3dd1d59f32f45656aecf4d9ce86cf0c4abef6866
Johnathan Rush <10775395+rushgeo@users.noreply.github.com>
2021-11-05 11:31:29 -0400

    combine model training and CV report rendering

--- R/run.R
+++ R/run.R
@@ -3,9 +3,6 @@
 # The second part for models is run sequentially by targets, since XGBoost
 # uses OpenMP and all cores by default to parallelize work.
 
-# Note the use of `shortcut = TRUE` means we're relying on checks against
-# tar_outdated() to avoid mismatched dependencies
-
 library(targets)
 
 # 1. Prepare predictors ####
@@ -23,21 +20,7 @@ if(any(unfinished)){
   stop('Aborting workflow. The following targets from part 1 need to complete before running part 2:\n',
        paste('  ', sort(outdated[unfinished]), collapse = '\n'))}
 
-# 2. Train models ####
+# 2. Train models and render CV report ####
 message('Starting part 2 sequentially at ', Sys.time())
-end_part2 = 'initial_cv_'
-tar_make(names = starts_with(!!end_part2), shortcut = TRUE)
+tar_make(names = initial_cv_report)
 message('Finished part 2 at ', Sys.time())
-
-# check if training was successful
-message('Checking status of part 2 before starting part 3...')
-outdated = tar_outdated()
-unfinished = grep(end_part2, outdated)
-if(any(unfinished)){
-  stop('Aborting workflow. The following targets from part 2 need to complete before running part 3:\n',
-       paste('  ', sort(outdated[unfinished]), collapse = '\n'))}
-
-# 3. Render report ####
-message('Rendering report at ', Sys.time())
-tar_make(names = initial_cv_report, shortcut = TRUE)
-message('Done')
