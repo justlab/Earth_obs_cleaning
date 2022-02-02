e00a33e4dd2f3e74fcc4189f53a89d3c058cb837
Johnathan Rush <10775395+rushgeo@users.noreply.github.com>
2022-02-02 13:08:36 -0500

    don't need target-level SF load right now

--- R/globals.R
+++ R/globals.R
@@ -25,9 +25,9 @@ vars0 <- c("Date(dd:mm:yyyy)", "Time(hh:mm:ss)", "Day_of_Year","AERONET_Site_Nam
            "Site_Elevation(m)", "Ozone(Dobson)", "NO2(Dobson)",
            "Solar_Zenith_Angle(Degrees)", "Precipitable_Water(cm)")
 
-# temporary AOI for testing prediction table preparation
-library(sf)
 crs_sinu = '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs'
-dc_wgs = st_bbox(c(xmin = -77.78, xmax = -76.45, ymax = 39.27, ymin = 38.52), crs = 4326)
-dc_sinu = st_bbox(st_transform(st_as_sfc(dc_wgs), crs_sinu))
-rm(dc_wgs)
+# # temporary AOI for testing prediction table preparation
+# library(sf)
+# dc_wgs = st_bbox(c(xmin = -77.78, xmax = -76.45, ymax = 39.27, ymin = 38.52), crs = 4326)
+# dc_sinu = st_bbox(st_transform(st_as_sfc(dc_wgs), crs_sinu))
+# rm(dc_wgs)
