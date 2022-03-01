db0efe84336c17cb3ee2439024e115311db77120
Kodi Arfer <git@arfer.net>
2022-03-01 11:24:08 -0500

    Revert "only train 2003 for quick testing of prediction"
    This reverts commit 834f430470b74963de21474bdaf72245269904f2.

--- _targets.R
+++ _targets.R
@@ -45,8 +45,7 @@ source('R/xgboost_cv_RFE.R')
 # Targets ####
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#process_years = 2003:2019
-process_years = 2003
+process_years = 2003:2019
 region_values = list(regions = aoiname)
 date_table = dates_year(process_years)
 sat_values = list(sat = sats)
