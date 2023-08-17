42a71dd5ec018446d45dfad6d3c19cd71344c2b8
Kodi Arfer <git@arfer.net>
2023-08-17 09:00:20 -0400

    Upgraded `Just.universal`.

--- code/libraries.R
+++ code/libraries.R
@@ -7,5 +7,15 @@ suppressPackageStartupMessages(
     library(xgboost)
     library(data.table)
     library(pbapply)})
+
 loadNamespace("fst")    # Used for targets with `format = "fst_dt"`
 loadNamespace("quarto") # Used by `tarchetypes::tar_quarto`
+
+# These are used by `xgboost.dart.cvtune`.
+loadNamespace("ParamHelpers")
+loadNamespace("lhs")
+
+# These are used by `download`.
+loadNamespace("DBI")
+loadNamespace("RSQLite")
+loadNamespace("digest")
